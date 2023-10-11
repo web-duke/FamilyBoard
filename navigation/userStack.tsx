aec246223dad3872d3296ee01de52cfd22407b0b
web-duke <xwebdukex@gmail.com>
2023-10-10 22:11:04 -0400

    drawer

--- navigation/userStack.tsx
+++ navigation/userStack.tsx
@@ -3,8 +3,19 @@ import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import Home from "../src/screens/Home";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  const { t } = useTranslation();
+  return (
+    <Drawer.Navigator initialRouteName={t("home")}>
+      <Drawer.Screen name={t("home")} component={Home} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function UserStack() {
   const { t } = useTranslation();
@@ -12,7 +23,7 @@ export default function UserStack() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name={t("home")} component={Home} />
+        <Stack.Screen name={t("familyBoard")} component={DrawerNavigator} />
       </Stack.Navigator>
     </NavigationContainer>
   );
